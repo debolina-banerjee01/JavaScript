b74d1717d7280debc758f8881c0b07933b82a3bd
debolina-banerjee01 <151387345+debolina-banerjee01@users.noreply.github.com>
2024-06-10 23:24:34 +0530

    Create area_perimeter_rectangle.js
    The length & breadth of a rectangle and radius of a circle are input through the keyboard. Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.

---
+++ area_perimeter_rectangle.js
@@ -0,0 +1,17 @@
+/* 
+chp1- F)b)   The length & breadth of a rectangle and radius of a circle are input through the keyboard. Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.
+*/
+
+let length = Number(prompt("Please enter length of the rectangle :"));
+let breadth = Number(prompt("Please enter breadth of the rectangle :"));
+let radius = Number(prompt("Please enter radius of the circle :"));
+
+const rArea = length * breadth;
+const rPeri = 2 * (length + breadth);
+const cArea = 3.14 * (radius * radius);
+const cCircum = 2 * 3.14 * radius;
+
+console.log(`Area of the rectangle is ${rArea}`);
+console.log(`Perimeter of the rectangle is ${rPeri}`);
+console.log(`Area of the circle is ${cArea}`);
+console.log(`Circumference of the cicle is ${cCircum}`);
