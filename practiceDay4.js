b549e37ccc3d6d8994a53bcdbf601e0abb30fc45
debolina-banerjee01 <151387345+debolina-banerjee01@users.noreply.github.com>
2024-06-20 22:59:03 +0530

    Add files via upload
    Practicing 'Arithmatic operators' (addition, subtraction, multiplication) from the book 'Javascript from Beginner to Professional'(page: 32)

---
+++ practiceDay4.js
@@ -0,0 +1,20 @@
+/*
+Practicing 'Arithmatic operators' from the book 'Javascript from Beginner to Professional'(page: 32)
+*/
+//addition
+let str1 = "Hello";
+let str2 = "addition";
+let result = str1 + " " + str2;
+console.log(result);
+
+//subtraction
+let str = "Hi";
+let no = 6;
+let result2 = str - no;
+console.log(result2);
+
+//multiplication
+let str3 = "Hi";
+let no2 = 3;
+let result3 = str3 * no2;
+console.log(result3);
